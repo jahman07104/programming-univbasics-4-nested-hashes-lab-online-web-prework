179bda21413d088d7713fc3a7db521979df5e118
Patrick Harrison <jahman@bellatlantic.net>
2019-06-30 18:47:48 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -11,7 +11,7 @@ def hopper
           :languages => ["Smalltalk", "LISP"]
         },
         :dennis_ritchie => {
-          :known_for => "Unix",
+          :known_for => ["Unix"],
           :languages => ["C"]
         }
      }
@@ -19,7 +19,7 @@ def hopper
 
 end
 
-def alan_kay_is_known_for =>["Object Orientation","small talk","LISP"]
+def alan_kay_is_known_for
 	
 	
 	programmer_hash = 
@@ -47,7 +47,7 @@ def dennis_ritchies_language
           :languages => ["COBOL", "FORTRAN"]
         },
         :alan_kay => {
-          :known_for => "Object Orientation",
+          :known_for => ["Object Orientation"],
           :languages => ["Smalltalk", "LISP"]
         },
         :dennis_ritchie => {
@@ -65,6 +65,12 @@ def adding_matz
 # }
 
 	programmer_hash = 
+	{
+	  :yukihiro_matsumoto => {
+	    :known_for => "Ruby",
+	    :languages => ["LISP","C"]
+	  },
+	}
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -92,13 +98,17 @@ def changing_alan
         },
         :alan_kay => {
           :known_for => "Object Orientation",
+          :knnown_for [:GUI]
           :languages => ["Smalltalk", "LISP"]
+          
         },
+      
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
         }
      }
+     
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
